e780e265be2895c08341e42bfff5a95701e2c719
VeronikaKohouskova <veronika.kohouskova@uhk.cz>
2021-10-30 16:16:02 +0200

    Úprava struktur - třída pro bod a úsečku.

--- task1+2/src/model/Point.java
+++ task1+2/src/model/Point.java
@@ -2,7 +2,7 @@ package model;
 
 public class Point {
 
-    public int x, y;
+    private int x, y;
 
     public Point(int x, int y) {
         this.x = x;
@@ -13,7 +13,24 @@ public class Point {
         this.x = (int) Math.round(x);
         this.y = (int) Math.round(y);
     }
+    public Point(Point p) {
+        x = p.x;
+        y = p.y;
+    }
 
-    //TODO
+    public int getX() {
+        return x;
+    }
 
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
