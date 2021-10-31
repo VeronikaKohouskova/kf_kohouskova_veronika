099fb167f057ea72f23e778750f1aa00bebc1742
VeronikaKohouskova <veronika.kohouskova@uhk.cz>
2021-10-31 17:54:51 +0100

    Úprava DDA algoritmu, aby fungoval ve všech kvandrantech.

--- task1+2/src/rasterize/DashAndDotLineRasterizer.java
+++ task1+2/src/rasterize/DashAndDotLineRasterizer.java
@@ -9,20 +9,42 @@ public class DashAndDotLineRasterizer extends LineRasterizer {
     @Override
     public void rasterize(int x1, int y1, int x2, int y2, int color) {
         int err, dx, dy;
-        dx = x2 - x1;
-        dy = y2 - y1;
+        int a = 0;
         err = 0;
 
-        do {
-            raster.setPixel(x1, y1, 0xff00ff);
-            if (err > 0) {
-                x1 = x1 + 1;
-                err = err - dy;
-            } else {
-                y1 = y1 + 1;
-                err = err + dx;
-            }
-        } while ((x1 <= x2) && (y1 <= y2));
+        if (x1>x2) {
+            int t = x1;
+            x1 = x2;
+            x2 = t;
+            t = y1;
+            y1 = y2;
+            y2 = t;
+        }
+        dx = x2 - x1;
+        dy = y2 - y1;
+        if (dy<0) {
+            do {
+                raster.setPixel(x1, y1, 0xff00ff);
+                if (err > 0) {
+                    x1 = x1 + 1;
+                    err = err - Math.abs(dy);
+                } else {
+                    y1 = y1 - 1;
+                    err = err + dx;
+                }
+            } while (x1 <= x2 && y2 <= y1);
 
+        } else {
+            do {
+                raster.setPixel(x1, y1, 0xff00ff);
+                if (err > 0) {
+                    x1 = x1 + 1;
+                    err = err - dy;
+                } else {
+                    y1 = y1 + 1;
+                    err = err + dx;
+                }
+            } while (x1 <= x2 && y1 <= y2);
+        }
     }
 }
