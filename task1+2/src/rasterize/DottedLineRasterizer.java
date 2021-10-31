5ee9c4285dc74d3bd3c140163b29269b4bd07336
VeronikaKohouskova <veronika.kohouskova@uhk.cz>
2021-10-31 20:07:06 +0100

    Úprava čáry na tečkovanou.

--- task1+2/src/rasterize/DottedLineRasterizer.java
+++ task1+2/src/rasterize/DottedLineRasterizer.java
@@ -8,40 +8,12 @@ public class DottedLineRasterizer extends LineRasterizer {
 
     @Override
     public void rasterize(int x1, int y1, int x2, int y2, int color) {
-        int a, x, y, p, xk1, xk2, dx, dy, yk1, yk2;
-        dx = x2 - x1;
-        dy = y2 - y1;
-        a = 0;
-        x = x1;
-        y = y1;
-        p = 2 * dy - dx;
-        xk1 = 2 * dy;
-        xk2 = 2 * (dy - dx);
-        yk1 = 2 * dx;
-        yk2 = 2 * (dx - dy);
-        raster.setPixel(x, y, color);
-        if (Math.abs(dy) < Math.abs(dx)) {
-            if (x2 < x1) {
-                int t = x1;
-                x1 = x2;
-                x2 = t;
-                t = y1;
-                y1 = y2;
-                y2 = t;
-            }
-                while (x < x2) {
-                    x = x + 1;
-                    if (p < 0) {
-                        p = p + xk1;
-                    } else {
-                        p = p + xk2;
-                        y = y + 1;
-                    }
-                    raster.setPixel(x, y, 0xff0000);
-                }
+        float k = (y2 - y1) / (float) (x2 - x1);
+        float q = y1 - k * x1;
+        int a = 0;
 
-        } else {
-            if (y2<y1) {
+        if (k<1 && k>-1) {
+            if(x2<x1) {
                 int t = x1;
                 x1 = x2;
                 x2 = t;
@@ -49,17 +21,30 @@ public class DottedLineRasterizer extends LineRasterizer {
                 y1 = y2;
                 y2 = t;
             }
-            while (y < y2) {
-                y = y + 1;
-                if (p <  0) {
-                    p = p + yk1;
-                } else {
-                    p = p + yk2;
-                    x = x + 1;
+        for (int x = x1; x <= x2; x++) {
+            float y = k * x + q;
+            a++;
+            if (a%5 == 0) {
+                raster.setPixel(x, Math.round(y), color);
+            }
+            }
+        } else {
+            if(y2<y1) {
+                int t = x1;
+                x1 = x2;
+                x2 = t;
+                t = y1;
+                y1 = y2;
+                y2 = t;
+            }
+            for (int y = y1; y<=y2; y++) {
+                float x = (y - q) / k;
+                a++;
+                if (a%5 == 0) {
+                    raster.setPixel(Math.round(x),y, color);
                 }
-                raster.setPixel(x, y, 0x00ff00);
             }
         }
     }
-
 }
+
