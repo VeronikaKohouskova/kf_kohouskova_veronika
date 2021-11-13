ed51fe40b89b717206e32da4a9bdd5e6792f289d
VeronikaKohouskova <veronika.kohouskova@uhk.cz>
2021-11-13 18:57:34 +0100

    Dohotovení třídy SeedFillBorder, která teď dědí od třídy SeedFill. Pracuje podobně, ale má jinak zadanou hranici a upravenou metodu isValidForFill.

--- task1+2/src/fill/SeedFillBorder.java
+++ task1+2/src/fill/SeedFillBorder.java
@@ -1,10 +1,27 @@
 package fill;
 
+import model.Point;
+import model.Polygon;
+import rasterize.Raster;
 
-public class SeedFillBorder implements Filler {
+public class SeedFillBorder extends SeedFill implements Filler {
 
-    @Override
-    public void fill() {
+    private int borderColor;
 
+    public SeedFillBorder(Raster raster) {
+        super(raster);
+    }
+
+    public void setSeed(Point seed, Polygon polygon) {
+        seedX = seed.getX();
+        seedY = seed.getY();;
+        borderColor = polygon.getColor();
+        backgroundColor = raster.getPixel(seed.getX(), seed.getY());
+    }
+
+   public boolean isValidForFill(Point point){
+        return point.getX() > 0 && point.getY() > 0 && point.getX() < raster.getWidth() && point.getY()
+                < raster.getHeight() && borderColor != raster.getPixel(point.getX(), point.getY()) &&
+                fillColor != raster.getPixel(point.getX(), point.getY()) && fillColor != borderColor;
     }
 }
