f6bdb20c30916a79712c44527d7b8e827b60b5ab
VeronikaKohouskova <veronika.kohouskova@uhk.cz>
2021-11-13 18:48:19 +0100

    Dodělání metod do třídy triangle, která dědí od polygonu.

--- task1+2/src/model/Triangle.java
+++ task1+2/src/model/Triangle.java
@@ -1,53 +1,61 @@
 package model;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Triangle extends Polygon{
 
-    private final int x1, y1, x2, y2, x3, y3;
-    private final int color;
 
-    public Triangle(int x1, int y1, int x2, int y2, int x3, int y3, int color) {
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
-        this.x3 = x3;
-        this.y3 = y3;
-        this.color = color;
+    public Triangle() {
+        this(new ArrayList<>());
     }
 
-  //  public Line(Point p1, Point p2, int color) {
-   //     this.p1 = p1;
-   //     this.p2 = p2;
-   //     this.color = color;
-
-  // }
-
-    public int getX1() {
-        return x1;
+    public Triangle(List<Point> points) {
+        this(points, Color.MAGENTA.getRGB());
     }
 
-    public int getY1() {
-        return y1;
+    public Triangle(List<Point> points, int color) {
+     super(points, color);
     }
 
-    public int getX2() {
-        return x2;
+    public Point getThirdPoint(Point point) {
+        if (points.size() < 2){
+            return null;
+        }
+        Point center = getCenter();
+        double centerX = center.getX();
+        double centerY = center.getY();
+
+
+        double r = (Math.sqrt(Math.pow((points.get(1).getX()-points.get(0).getX()),2) + Math.pow((points.get(1).getY()-points.get(0).getY()),2))/2);
+        double l = (Math.sqrt(Math.pow(point.getX()-centerX,2) + Math.pow(point.getY()-centerY,2)));
+        double ratio = r/l;
+        double newX = centerX+(point.getX()-centerX)*ratio;
+        double newY = centerY+(point.getY()-centerY)*ratio;
+        return new Point(newX,newY);
+
     }
 
-    public int getY2() {
-        return y2;
+    public Point getCenter() {
+        double centerX = (points.get(0).getX()+points.get(1).getX())/2.0;
+        double centerY = (points.get(0).getY()+points.get(1).getY())/2.0;
+
+        return new Point(centerX,centerY);
     }
 
-    public int getX3() {
-        return x3;
+    public int getX1(){
+     return points.get(0).getX();
+    }
+    public int getX2(){
+        return points.get(1).getX();
+    }
+    public int getY1(){
+        return points.get(0).getY();
     }
 
-    public int getY3() {
-        return y3;
-    }
-
-    public int getColor() {
-        return color;
+    public int getY2(){
+        return points.get(1).getY();
     }
 
 }
