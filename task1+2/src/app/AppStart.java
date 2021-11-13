e08fc96e0abe32bfc53fb5b3b03dcc5cc6587603
VeronikaKohouskova <veronika.kohouskova@uhk.cz>
2021-11-13 17:48:42 +0100

    Nastavení plné úsečky jako výchozí objekt, který se vykresluje.

--- task1+2/src/app/AppStart.java
+++ task1+2/src/app/AppStart.java
@@ -1,6 +1,7 @@
 package app;
 
 import control.Controller2D;
+import rasterize.FilledLineRasterizer;
 import view.Window;
 
 import javax.swing.*;
@@ -9,9 +10,10 @@ public class AppStart {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            Window window = new Window();
+           Window window = new Window();
             Controller2D controller = new Controller2D(window.getPanel());
             window.setController(controller);
+            controller.changeRasterizer(new FilledLineRasterizer(controller.getRaster()));
             window.setVisible(true);
         });
         // https://www.google.com/search?q=SwingUtilities.invokeLater
