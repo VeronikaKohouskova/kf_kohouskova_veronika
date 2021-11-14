ad503b19ebef8af11f4144b81a92839a3fa8a990
VeronikaKohouskova <veronika.kohouskova@uhk.cz>
2021-11-14 18:17:39 +0100

    Úprava třídy Clipper - dohotovení metody clip a doplnění metody, která vytváří hrany.

--- task1+2/src/rasterize/Clipper.java
+++ task1+2/src/rasterize/Clipper.java
@@ -1,7 +1,11 @@
 package rasterize;
 
+import model.Edge;
+import model.Line;
 import model.Point;
+import model.Polygon;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,21 +17,38 @@ public class Clipper {
      * @param clipPolygon ořezávací polygon
      * @return seznam bodů ořezaného polygonu
      */
-    public static List<Point> clip(List<Point> polygon, List<Point> clipPolygon) {
 
-        List<Point> in = new ArrayList<>(polygon);
+    public List<Point> clip(Polygon polygon, Polygon clipPolygon) {
+        List<Edge> clipPolygonEdges = new ArrayList<>(convertToEdges(polygon.getLines()));
+        List<Point> in = new ArrayList<>(polygon.getPoints());
+        List<Point> out = new ArrayList<>();
 
-        Point p1 = null; // vložit poslední clip point
-        for (Point p2 : clipPolygon) {
-            List<Point> out = new ArrayList<>();
-            // vytvořit hranu z bodů p1 a p2
-            // Point v1 = in.lastx
-            for (Point v2 : in) {
-                // TODO algoritmus
+        for (Edge edge: clipPolygonEdges) {
+            out.clear();
+            Point v1 = in.get(in.size()-1);
+            for ( Point v2: in) {
+                if(edge.isInside(v2)) {
+                    if (!edge.isInside(v1)) {
+                        out.add(edge.getIntersection(v1,v2));
+                        out.add(v2);
+                    }else {
+                        if(edge.isInside(v1));
+                        out.add(edge.getIntersection(v1,v2));
+                    }
+                    v1 = v2;
+                }
             }
-            p1 = p2;
             in = out;
         }
         return in;
+    }
+
+    public static List<Edge> convertToEdges(List<Line> lines) {
+        List<Edge> edges = new ArrayList<>();
+        for (Line line : lines) {
+            Edge edge = new Edge(line.getP1(), line.getP2(), line.getColor());
+            edges.add(edge);
+        }
+        return edges;
     }
 }
